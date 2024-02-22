4b11e61b6c9451c269dab4daf55b12963100b500
DESKTOP-00P3NR3\jotan <jotanvo@gmail.com>
2024-02-22 23:47:11 +0100

    ee

--- productpage/js/product.js
+++ productpage/js/product.js
@@ -7,11 +7,23 @@ fetch(API_URL,)
     .then(movies => {
         console.log(movies); {
             container.innerHTML += `
-        <div>
-            <span>${movies[0].title}</span>
+        
+            <div>
+                <img src="${movies[0].image}">
             
+                
+            </div>
             
-        </div>
+            <div id="movie-info">
+            <div>${movies[0].title}<span>(${movies[0].released})</span></div>
+            <div>${movies[0].description}</div>
+                <div>Rating</div>
+                <div>${movies[0].rating}</div>
+                <div>Genre</div>
+                <div>${movies[0].genre}</div>
+            </div>
+           
+        
             `
         }
 
